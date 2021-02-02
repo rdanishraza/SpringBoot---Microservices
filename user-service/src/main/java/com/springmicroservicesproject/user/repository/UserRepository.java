b9ede0e63bfc517afb3c361c1390e5f132410266
Danish Raza <rdanishraza@gmail.com>
2021-02-02 12:14:52 +0530

    It is another microservice which will communicate with previous microservice through POST and GET request

---
+++ user-service/src/main/java/com/springmicroservicesproject/user/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.springmicroservicesproject.user.repository;
+
+import com.springmicroservicesproject.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository  extends JpaRepository<User,Long> {
+    User findByUserId(Long userId);
+}
